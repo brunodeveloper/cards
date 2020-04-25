8aa345efe1ce02e9712e6427e1654d48b4e70e69
Bruno Carvalho <brunodeveloper7@gmail.com>
2020-04-25 00:36:59 -0300

    feat: add ex_doc

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,13 +1,18 @@
 defmodule Cards do
+  @moduledoc """
+    Provider methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a list of string representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for value <- values, suit <- suits do
-        "#{value} of #{suit}"
+      "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
@@ -18,6 +23,19 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+    ## Examples
+
+        iex> deck = Cards.create.deck
+        iex> {hand, deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -27,4 +45,16 @@ defmodule Cards do
     File.write(filename, binary)
   end
 
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
 end
